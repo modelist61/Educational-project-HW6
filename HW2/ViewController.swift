f7a134b9672d43b37dae7b3452ffd6e45d6447dc
Tokarev Dmitry <modelist61@bk.ru>
2020-10-19 14:44:21 +0300

    final code

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -23,38 +23,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         multiColorView.layer.cornerRadius = 15
-        
-        sliderRed.minimumValue = 0.00
-        sliderRed.maximumValue = 1.00
-        sliderRed.minimumTrackTintColor = .red
-        
-        sliderGreen.minimumValue = 0.00
-        sliderGreen.maximumValue = 1.00
-        sliderGreen.minimumTrackTintColor = .green
-
-        sliderBlue.minimumValue = 0.00
-        sliderBlue.maximumValue = 1.00
-        sliderBlue.minimumTrackTintColor = .blue
+        multiColorView.backgroundColor = UIColor(displayP3Red: CGFloat(sliderRed.value),
+                                                 green: CGFloat(sliderGreen.value),
+                                                 blue: CGFloat(sliderBlue.value), alpha: 1.0)
     }
 
     @IBAction func redSliderAction() {
         redValue.text = String(format: "%.2f", sliderRed.value)
-        rgbLable(red: sliderRed.value, green: sliderGreen.value, blue: sliderBlue.value)
+        rgbLable()
     }
     @IBAction func greenSliderAction() {
         greenValue.text = String(format: "%.2f", sliderGreen.value)
-        rgbLable(red: sliderRed.value, green: sliderGreen.value, blue: sliderBlue.value)
+        rgbLable()
     }
     @IBAction func blueSliderAction() {
         blueValue.text = String(format: "%.2f", sliderBlue.value)
-        rgbLable(red: sliderRed.value, green: sliderGreen.value, blue: sliderBlue.value)
+        rgbLable()
     }
 }
 
 // MARK: - RGB mixing
 extension ViewController {
-    private func rgbLable(red: Float, green: Float, blue: Float) {
-        multiColorView.backgroundColor = UIColor(displayP3Red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
+    private func rgbLable() {
+        multiColorView.backgroundColor = UIColor(displayP3Red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1.0)
     }
 }
+
+
 
